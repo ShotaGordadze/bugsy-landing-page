246de1811f22e6d714b48845f1c23698110af762
ShotaGordadze <sh.gordadze@rsig.ge>
2025-02-25 17:11:16 +0400

    2/25/25

--- bugsy-ui/js/script.js
+++ bugsy-ui/js/script.js
@@ -1,52 +1,6 @@
-document.querySelector('.info-one').onmousemove = (e) => {
-    const rect = e.target.getBoundingClientRect();
-    const x = e.clientX - rect.left; // Mouse X inside element
-    const y = e.clientY - rect.top;  // Mouse Y inside element
-
-    e.target.style.setProperty('--x', `${x}px`);
-    e.target.style.setProperty('--y', `${y}px`);
-};
-
-document.querySelector('.info-two').onmousemove = (e) => {
-    const rect = e.target.getBoundingClientRect();
-    const x = e.clientX - rect.left; // Mouse X inside element
-    const y = e.clientY - rect.top;  // Mouse Y inside element
-
-    e.target.style.setProperty('--x', `${x}px`);
-    e.target.style.setProperty('--y', `${y}px`);
-};
-
-
-document.querySelector('.info-three').onmousemove = (e) => {
-    const rect = e.target.getBoundingClientRect();
-    const x = e.clientX - rect.left; // Mouse X inside element
-    const y = e.clientY - rect.top;  // Mouse Y inside element
-
-    e.target.style.setProperty('--x', `${x}px`);
-    e.target.style.setProperty('--y', `${y}px`);
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-    const infoSections = document.querySelectorAll(".info");
-  
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            entry.target.style.opacity = "1";
-            entry.target.style.transform = "translateY(0)";
-          } else {
-            entry.target.style.opacity = "0";
-            entry.target.style.transform = "translateY(50px)";
-          }
-        });
-      },
-      { threshold: 0.2 } // Trigger when 20% of the section is visible
-    );
-  
-    infoSections.forEach(section => observer.observe(section));
-  });
-  
+    function flipCard(card) {
+    card.classList.toggle("flipped");
+  }
 
 document.querySelector('.shiny-cta').addEventListener('click', function() {
     window.location.href = 'https://docs.google.com/forms/d/1yUad3S1KrFy0hZ6TJxbNnPmmOLz7B80mnxQZR961kHs/viewform?pli=1&pli=1&edit_requested=true';  // Replace with your desired URL
