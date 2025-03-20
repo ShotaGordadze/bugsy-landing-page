006e36ad3a7222ce2b4b01322d9e859eee140234
ShotaGordadze <sh.gordadze@rsig.ge>
2025-03-20 13:22:43 +0400

    s

--- bugsy-ui/js/script.js
+++ bugsy-ui/js/script.js
@@ -2,6 +2,10 @@
     card.classList.toggle("flipped");
   }
 
-document.querySelector('.shiny-cta').addEventListener('click', function() {
-    window.location.href = 'https://docs.google.com/forms/d/1yUad3S1KrFy0hZ6TJxbNnPmmOLz7B80mnxQZR961kHs/viewform?pli=1&pli=1&edit_requested=true';  // Replace with your desired URL
+  document.querySelector('.shiny-cta').addEventListener('click', () => {
+    window.open(
+      'https://drive.google.com/file/d/1uoXeN7bg4h_D961dk_aFUpOnJKL0IHOJ/view?usp=sharing', 
+      '_blank' // opens in a new tab
+    );
   });
+  
